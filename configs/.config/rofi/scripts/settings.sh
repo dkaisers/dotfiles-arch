e234eb3857701ddb90e93fc946583dbd0fbc9710
Dominik Kaisers <dominik@dkaisers.net>
2026-02-23 23:44:28 +0100

    Fix settings menu

--- configs/.config/rofi/scripts/settings.sh
+++ configs/.config/rofi/scripts/settings.sh
@@ -4,13 +4,13 @@ ICON_DIR="$HOME/.assets/lucide/icons"
 
 case "$1" in
 "Audio")
-  hyprctl dispatch exec '[float; center; size 1200 900] kitty -e impala' >/dev/null 2>&1
+  hyprctl dispatch exec '[float; center; size 1200 900] kitty -e wiremix --tab output' >/dev/null 2>&1
   ;;
 "Bluetooth")
   hyprctl dispatch exec '[float; center; size 1200 900] kitty -e bluetui' >/dev/null 2>&1
   ;;
 "Wifi")
-  hyprctl dispatch exec '[float; center; size 1200 900] kitty -e wiremix --tab output' >/dev/null 2>&1
+  hyprctl dispatch exec '[float; center; size 1200 900] kitty -e impala' >/dev/null 2>&1
   ;;
 *)
   echo -e "Audio\0icon\x1f$ICON_DIR/volume-2.svg"
